6c99b59984a77503b942494734e2a92bea26df9b
Misael Borges <ml_bbd@hotmail.com>
2024-05-06 15:48:04 -0300

    Métodos e Variaveis

---
+++ metodos-variaveis/src/MetodosVariaveis.java
@@ -0,0 +1,13 @@
+public class MetodosVariaveis {
+    public static void main(String[] args) {
+        String primeiroNome = "Misael";
+        String segundoNome = "Borges";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método é:" + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
